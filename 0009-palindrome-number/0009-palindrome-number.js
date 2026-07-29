9e0a2dcfd4107de95372d8b83c64269f78e3345e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-07-29 22:01:14 +0530

    Time: 5 ms (77.68%), Space: 63.2 MB (81.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    // Negative numbers and numbers ending with 0
+    // (except 0 itself) cannot be palindromes.
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
+    }
+
+    let reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    // Even digits: x === reversedHalf
+    // Odd digits: x === Math.floor(reversedHalf / 10)
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+};
