4245edd2b06959ae48f5293db3ec66f1ea7417e7
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-06 11:22:39 +0530

    Time: 1 ms (28.88%), Space: 53.2 MB (80.02%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,33 @@
+var letterCombinations = function(digits) {
+    if (digits.length === 0) return [];
+
+    const map = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    };
+
+    const result = [];
+
+    function backtrack(index, current) {
+        if (index === digits.length) {
+            result.push(current);
+            return;
+        }
+
+        const letters = map[digits[index]];
+
+        for (let ch of letters) {
+            backtrack(index + 1, current + ch);
+        }
+    }
+
+    backtrack(0, "");
+
+    return result;
+};
